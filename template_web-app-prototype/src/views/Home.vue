0c31f0bde11a5abb3c6e32d5ff1e1bc54aa2543d
Ваше Имя <you@example.com>
2021-04-15 18:23:53 -0300

    temp-state

---
+++ template_web-app-prototype/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <div class="home">
+    <img alt="logo" src="../assets/logo.svg" class="">
+
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {},
+};
+
+</script>
